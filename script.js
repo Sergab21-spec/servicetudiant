49a07e5f088726a452bb98733892cb106eef630c
matlappp <154023456+matlappp@users.noreply.github.com>
2026-01-02 21:12:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 console.log("SCRIPT LOADED");
 
 const button = document.getElementById("openDropdown");
-const dropdown = document.getElementById("dropdown");
+const dropdown = document.getElementById("navWrap");
 
 console.log(button, dropdown);
 
 if (button && dropdown) {
   button.addEventListener("click", () => {
-    console.log("BUTTON CLICKED");
     dropdown.classList.toggle("open");
     console.log(dropdown.className);
   });
