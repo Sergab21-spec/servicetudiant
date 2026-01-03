468fdbb9a64e37588e1c21d39b94df1969f02813
matlappp <154023456+matlappp@users.noreply.github.com>
2026-01-02 20:56:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -11,17 +11,3 @@ if (button && dropdown) {
         dropdown.classList.toggle("dropdown-open");
     });
 }
-const carousel = document.getElementById('testimonialCarousel');
-const testimonials = document.querySelectorAll('.testimonial');
-let current = 0;
-
-function showTestimonial(index) {
-  const offset = -index * 100; // décalage en %
-  carousel.style.transform = `translateX(${offset}%)`;
-}
-
-// Auto-défilement toutes les 5 secondes
-setInterval(() => {
-  current = (current + 1) % testimonials.length;
-  showTestimonial(current);
-}, 5000);
