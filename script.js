1ed5fe5194a2321116b98b7e5888a2d174ac7d0a
Sergab21-spec <Sergab21@hotmail.com>
2026-01-11 20:48:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -82,6 +82,19 @@ if (form) {
     }
   });
 }
+console.log("SCRIPT LOADED");
+
+const button = document.getElementById("openDropdown");
+const dropdown = document.getElementById("navWrap");
+
+console.log(button, dropdown);
+
+if (button && dropdown) {
+  button.addEventListener("click", () => {
+    dropdown.classList.toggle("open");
+    console.log(dropdown.className);
+  });
+}
 
 
 
